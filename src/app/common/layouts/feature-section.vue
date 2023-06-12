3b40b5971a49414915340ff8c92a4db719ba1154
IzoIbrahim <israel@brrng.com>
2023-06-12 09:57:02 +0300

    Make overlapped section fade

--- src/app/common/layouts/feature-section.vue
+++ src/app/common/layouts/feature-section.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { ref, onMounted, onUnmounted } from 'vue';
-import { onIntersect } from '../../composables/on-intersect.composable'
+import { ref, onMounted, onUnmounted } from "vue";
+import { onIntersect } from "../../composables/on-intersect.composable";
 
 const leftCardObserver = ref({});
 const rightCardObserver = ref({});
@@ -21,29 +21,73 @@ const props = defineProps({
 });
 
 const onEnterLeftCard = () => {
-  leftCardRef.value.classList.add('left-card-bounce')
- //console.log("entered");
+  leftCardRef.value.classList.add("left-card-bounce");
+  //console.log("entered");
 };
 
 const onEnterRightCard = () => {
-  rightCardRef.value.classList.add('right-card-bounce')
+  rightCardRef.value.classList.add("right-card-bounce");
   console.log("entered");
 };
 
 const onExit = () => {
   // leftCardRef.value.classList.remove('left-card-bounce')
   // rightCardRef.value.classList.remove('right-card-bounce')
- console.log("exited");
+  console.log("exited");
 };
 
+/**FUNCTIONS */
+function handleScroll() {
+  const elements = document.querySelectorAll("#feature-section");
+ console.log(`Scroll handling ${elements.length}`)
+  for (let index = 0; index < 2; index++) {
+    const element = elements[index];
+    const element2 = elements[index + 1];
+    const rect2 = element2.getBoundingClientRect();
+    const rect = element.getBoundingClientRect();
+    const scrollPosition =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    if (rect.top == 40 || rect.top === 50 && rect2.top < rect.height) {
+      if (rect.top === 40 && element.style.opacity < 0.4) {
+        //const opacity = 1 - ((scrollPosition - rect.top) / rect.height) * 0.4;
+      } else if(rect.top === 50 && element.style.opacity < 0.7){
+        //
+      } else {
+        const opacity = 1 - ((scrollPosition - rect.top) / rect.height) * 0.2;
+        const scale = ((scrollPosition - rect.top) / rect.height) /10
+        element.style.opacity = opacity;
+        // element.style.transformOrigin='top';
+        // element.style.transform = `scale(${scale})`;
+      }
+    } else {
+      element.style.opacity = 1;
+      // element.style.transformOrigin='initial';
+      // element.style.transform=`scale(1)`;
+    }
+  }
+}
 
 onMounted(() => {
-  leftCardObserver.value = onIntersect(leftCardRef.value, onEnterLeftCard, onExit, false, {
-    threshold: 0.8,
-  });
-  rightCardObserver.value = onIntersect(leftCardRef.value, onEnterRightCard, onExit, false, {
-    threshold: 0.8,
-  });
+  leftCardObserver.value = onIntersect(
+    leftCardRef.value,
+    onEnterLeftCard,
+    onExit,
+    false,
+    {
+      threshold: 0.8,
+    }
+  );
+  rightCardObserver.value = onIntersect(
+    leftCardRef.value,
+    onEnterRightCard,
+    onExit,
+    false,
+    {
+      threshold: 0.8,
+    }
+  );
+  window.addEventListener('scroll', handleScroll)
 });
 
 // When the component is removed, disconnect the observer (clean-up step)
@@ -112,17 +156,17 @@ onUnmounted(() => {
               <slot name="featureFigure"></slot>
             </div>
             <div ref="rightCardRef">
-            <slot name="featureCard2"></slot>
-          </div>
+              <slot name="featureCard2"></slot>
+            </div>
           </div>
           <div
             v-if="uniqueFeature"
             id="feature-illustration"
             class="relative mx-auto mb-[70px] h-172px max-w-[280px] w-full md:h-[267px] md:max-w-full md:w-[412px] xl:w-[512px] xl:h-[332px]"
           >
-          <div ref="leftCardRef">
-            <slot name="featureCard1"></slot>
-          </div>
+            <div ref="leftCardRef">
+              <slot name="featureCard1"></slot>
+            </div>
             <div
               id="feature-figure"
               class="h-full overflow-hidden rounded-full"
@@ -140,53 +184,51 @@ onUnmounted(() => {
 </template>
 
 <style scoped>
-.left-card-bounce{
+.left-card-bounce {
   animation: leftCardBounce 1s ease-out 1;
-transition: opacity 2s;
+  transition: opacity 2s;
 }
 
 @keyframes leftCardBounce {
   0% {
-    transform:rotate(-10deg);
-    opacity: 0
+    transform: rotate(-10deg);
+    opacity: 0;
   }
   65% {
-    transform:rotate(5deg);
-    opacity: 0.3
+    transform: rotate(5deg);
+    opacity: 0.3;
   }
   95% {
-    tranform:rotate(-5deg);
-    opacity: 0.6
+    tranform: rotate(-5deg);
+    opacity: 0.6;
   }
   100% {
-    transform:rotate(0deg);
-    opacity: 1
+    transform: rotate(0deg);
+    opacity: 1;
   }
 }
 
-.right-card-bounce{
+.right-card-bounce {
   animation: rightCardBounce 1s ease-out 1;
-transition: opacity 2s;
+  transition: opacity 2s;
 }
 
 @keyframes rightCardBounce {
   0% {
-    transform:rotate(10deg);
-    opacity: 0
+    transform: rotate(10deg);
+    opacity: 0;
   }
   65% {
-    transform:rotate(-5deg);
-    opacity: 0.3
+    transform: rotate(-5deg);
+    opacity: 0.3;
   }
   95% {
-    tranform:rotate(5deg);
-    opacity: 0.6
+    tranform: rotate(5deg);
+    opacity: 0.6;
   }
   100% {
-    transform:rotate(0deg);
-    opacity: 1
+    transform: rotate(0deg);
+    opacity: 1;
   }
 }
-
-
 </style>
